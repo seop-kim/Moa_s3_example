17e05a38766be91aa3e33b9bf41f2c77456c06e4
seop <77163842+seop-kim@users.noreply.github.com>
2024-07-29 15:52:16 +0900

    init

---
+++ src/main/java/com/moa/MoaS3Example/MoaS3ExampleApplication.java
@@ -0,0 +1,13 @@
+package com.moa.MoaS3Example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoaS3ExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoaS3ExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moa/MoaS3Example/MoaS3ExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moa.MoaS3Example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoaS3ExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
